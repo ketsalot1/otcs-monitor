a8e9b45f94505df042eb1b55b082f6307e3d05c5
mr.martin.metal <mr.martin.metal@gmail.com>
2012-11-12 15:30:23 +0000

    Initial checkin

---
+++ app.js
@@ -0,0 +1,76 @@
+Ext.application({
+	name: 'itsm',
+
+	requires: [
+		'Ext.MessageBox'
+	],
+
+	models: ['itsm', 'desktopITSM', 'settings'],
+	stores: ['itsm', 'desktopITSM', 'settings'],
+	controllers: ['itsm'],
+	views: [
+		'MainListContainer', 
+		'itsmList', 
+		'itsmDetail', 
+		'ConfigurationView'
+	],
+
+	icon: {
+		'57': 'resources/icons/Icon.png',
+		'72': 'resources/icons/Icon~ipad.png',
+		'114': 'resources/icons/Icon@2x.png',
+		'144': 'resources/icons/Icon~ipad@2x.png'
+	},
+
+	isIconPrecomposed: true,
+
+	startupImage: {
+		'320x460': 'resources/startup/320x460.jpg',
+		'640x920': 'resources/startup/640x920.png',
+		'768x1004': 'resources/startup/768x1004.png',
+		'748x1024': 'resources/startup/748x1024.png',
+		'1536x2008': 'resources/startup/1536x2008.png',
+		'1496x2048': 'resources/startup/1496x2048.png'
+	 },
+
+	viewport: {
+		autoMaximize: true
+	},
+
+	launch: function() {
+		// Destroy the #appLoadingIndicator element
+		Ext.fly('appLoadingIndicator').destroy();
+
+		var mainListContainer = {
+				xtype: "mainlistcontainer"
+		};
+		var itsmList =	{
+				xtype: "itsmlist"
+		};
+		var itsmDetail =  {
+				xtype: "itsmdetail"
+		};
+		var configurationView = {
+				xtype: "configurationview"
+		};
+
+		Ext.getStore('settings').load();
+//		console.log( 'Main, retrieved conf: ' + Ext.getStore('settings').getAt(0).get('settingsContainer')[0] );
+
+		// Initialize the main view
+		// Ext.Viewport.add(itsmDetail);
+		Ext.Viewport.add([mainListContainer,itsmList,itsmDetail,configurationView]);
+	},
+
+	onUpdated: function() {
+		Ext.Msg.confirm(
+				"Application Update",
+				"This application has just successfully been updated to the latest version. Reload now?",
+				function(buttonId) {
+					if (buttonId === 'yes') {
+						window.location.reload();
+					}
+				}
+		);
+	}
+});
