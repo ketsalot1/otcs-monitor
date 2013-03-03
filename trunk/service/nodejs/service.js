992de3b1ffde54194480319e23c2c7d5dad72ecd
mr.martin.metal@gmail.com <mr.martin.metal@gmail.com@71220ff5-14bf-0be6-301b-028f0716ab84>
2013-03-03 18:15:42 +0000

    Continue improving the MongoDB support  - added handler for e-mail insert.

--- trunk/service/nodejs/service.js
+++ trunk/service/nodejs/service.js
@@ -1,10 +1,20 @@
 var http = require('http');
 var url = require('url');
+var _data = "/path?";
 
 function service(route,handler) {
 	http.createServer(function (req, res) {
+		var params;
+		req.on('data', function(chunk) {
+			_data += chunk;
+		});
 		req.on('end', function () {
-			var params = url.parse(req.url, true).query;
+			if( req.method == "POST" && _data.length > 7 ) {
+				params = url.parse( _data, true ).query;
+			} else {
+				params = url.parse(req.url, true).query;
+			}
+			_data = "/path?"; // clear the global variable before next request comes
 			route(handler,params,res); 
 		});
 	}).listen(38080, '127.0.0.1');
