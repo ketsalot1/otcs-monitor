55f8985b8a2e5dc6addab0408aa9963f787fec96
mr.martin.metal <mr.martin.metal@googlemail.com>
2013-09-02 20:19:16 +0000

    New parsing mechanism for node.js service. The new parsing is required for jQuerty Mobile and its spoecific way for formatting parameters by getJSON method.

--- service/nodejs/jQueryHandlers.js
+++ service/nodejs/jQueryHandlers.js
@@ -23,13 +23,17 @@ function getData( callback, pattern, res ) {
 	var R = {};
 	try {
 		R.responseText="OK";
-		R.responseCode=200;
-		R.data={};
-		R.data.counter=3;
-		R.data.feed=[];
-		R.data.feed[0]="Prvni text";
-		R.data.feed[1]="Druhy text";
-		R.data.feed[2]="Treti text";
+		R.responseStatus=200;
+		R.support_data={};
+		R.support_data.feed={};
+		R.support_data.feed.entries=[];
+		R.support_data.feed.entries[0]={"description":'0: OTCS Ticket 1669447 - WebViewer 10.2 patch -006 "PDFL Exception: 0x2001000e:Expected a dict object"'};
+		R.support_data.feed.entries[1]={"description":'1: OTCS Ticket 1699223 - Java Viewer 9.7 and 10 - problem with chosing colour of annotations on Japanese OS'};
+		R.support_data.feed.entries[2]={"description":'2: OTCS Ticket 1709907 - LLImaging module 10.3.0 causes Problems - URGENT'};
+		R.support_data.feed.entries[3]={"description":'3: OTCS Ticket 1713885 - WebViewer 10.2 - printlist export as plain .txt result in incomplete output.'};
+		R.support_data.feed.entries[4]={"description":'4: OTCS Ticket 1714608 - LLImaging Module 10.3 - Deichmann'};
+		R.support_data.feed.entries[5]={"description":'5: OTCS Ticket 1717501 - SAP & WebViewer 10.2 patch -006 - solving Document Title problem caused ERROR messages'};
+		R.support_data.feed.entries[6]={"description":'6: OTCS Ticket 1719136 - WebViewer 10.2 -006 .. sending .jpg format multi page files as mail attachments.'};
 	}
 	catch(e) {
  		logger.error('jQueryHandlers.getData: Error: ' + e.name + " - " + e.message );
--- service/nodejs/router.js
+++ service/nodejs/router.js
@@ -1,5 +1,9 @@
 function route(handle, params, res) {
 	console.log("router: got routing request");
+	var envelop=new RegExp("^.*\\[.*\\]$");
+	var idx=new RegExp("\\[.*\\]$");
+	var cutter=new RegExp("[a-zA-Z]\+");
+	var P = {};
 
 	try {
 		var callback = params['processSupportData'];
@@ -14,6 +18,30 @@ function route(handle, params, res) {
 		var cmd = params['cmd'];
 		var pars = params['data'];
 
+		if( typeof pars === 'undefined' ) {
+			for( var iterator in params ) {
+				if( envelop.exec(iterator) !== null ) {
+					temp = idx.exec(iterator);
+					if( temp !== null ) {
+						i = cutter.exec(temp[0]);
+						if( i !== null && i.length > 0){
+							ii = i[0];
+							P[ii]=params[iterator];
+						}
+					}
+				}
+			}
+			pars = JSON.stringify(P);
+		}
+		/* jQuery AJAX convinience method has a weird custom to reformat
+		 * the quesry sent to server. The JSON notation is reworked for 
+		 * object access strategy:
+		 *
+		 * Example:
+		 * 	cmd=c&data={"a":"1":"b":"nop"} ~ cmd=c&data[a]=0&data[b]=nop
+		 *
+		 * This code is a poor man example to reparse the URL ...
+		 */
 		if( typeof cmd == 'undefined' ) throw({"name":"Command not available the query"});
 		if (typeof handle[cmd] !== 'function') throw({"name":"Unknow command received"});
 		handle[cmd](callback,pars,res);
